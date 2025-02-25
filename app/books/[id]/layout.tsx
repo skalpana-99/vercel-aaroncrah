2480789d0a546804770e6b2b36850ca450bbdeb3
kalpana-bandara <kalpanabandara.info@gmail.com>
2025-02-25 18:14:30 +0530

    deleted books/layout

--- app/books/[id]/layout.tsx
+++
@@ -1,8 +0,0 @@
-export async function generateStaticParams() {
-  const slugs = [{ id: 1 }, { id: 2 }, { id: 3 }];
-  return slugs.map(({ id }) => ({ id: id.toString() }));
-}
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
